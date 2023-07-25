f931e713b811c7f4eca35a2e5b4f186079c07883
Giovanni Silva <no-reply@gsilva.pro>
2023-07-25 11:55:15 -0300

    try update with correct versions

--- PostGIS/update.sh
+++ PostGIS/update.sh
@@ -32,8 +32,8 @@ versions=("${versions[@]%/}")
 fetch_postgres_image_version() {
     local suite="$1";
     local item="$2";
-	curl -SsL "https://registry.hub.docker.com/v2/repositories/nickblah/postgis/tags/?name=${suite}&ordering=last_updated&" | \
-	  jq -c ".results[] | select( .name | match(\"^${suite}-([a-z|A-Z|.|-]+)?[0-9.]+$\"))" | \
+	curl -SsL "https://registry.hub.docker.com/v2/repositories/${repository}/postgis/tags/?name=${suite}&ordering=last_updated&page_size=100" | \
+	  jq -c ".results[] | select( .name | match(\"${suite}[0-9.]+-postgis-3$\"))" | \
 	  jq -r ".${item}" | \
 	  sort -r | \
 	  head -n1
